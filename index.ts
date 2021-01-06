d53c5ae647f59be54c47fbac61135ae84c4ccb73
Evan (Evangellos) Wiegers <evan.wiegers@usask.ca>
2021-01-05 19:28:30 -0600

    testing first pass

--- index.ts
+++ index.ts
@@ -2,16 +2,21 @@ import { PDFDocument } from 'pdf-lib';
 import * as fs from 'fs';
 
 const merge = async (name: string, ...pdfs: string[]) => {
+	console.log('starting');
 	if (pdfs.length < 2) throw new Error('Invalid numnber of arguments');
 
+	console.log('creating pdf');
 	const doc = await PDFDocument.create();
 
+	console.log('looping');
 	for (const pdfName of pdfs) {
+		console.log(`looping: ${pdfName}`);
 		const pdf = await PDFDocument.load(fs.readFileSync(pdfName));
 		const content = await doc.copyPages(pdf, pdf.getPageIndices());
 		content.forEach(doc.addPage);
 	}
 
+	console.log('writing file');
 	fs.writeFileSync(`./${name}`, await doc.save());
 };
 
